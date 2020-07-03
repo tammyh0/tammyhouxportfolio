8dba81fb629ac20289ef53ba164699c7f2f5adce
Tammy Ho <tammyh0@uw.edu>
2020-07-03 15:57:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/App.css'
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom'
+import {TinyButton as ScrollUpButton} from "react-scroll-up-button"
+
 
 function App() {
-  return (
+  return <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="App-logo">
+          <Link exact to="/">TAMMY HO</Link>
+        </div>
+
+        <div className="Nav-bar">
+            <NavLink exact className="Work-link" to="/" activeStyle={{
+              color: "#000000",
+              opacity: "0.8",
+              fontWeight: "600"}}>Work
+            </NavLink>
+
+            <NavLink className="About-link" to="/about" activeStyle={{
+              color: "#000000",
+              opacity: "0.8",
+              fontWeight: "600"}}>About
+            </NavLink>
+
+            <NavLink className="Resume-link" to="/resume" activeStyle={{
+              color: "#000000",
+              opacity: "0.8",
+              fontWeight: "600"}}>Resume
+            </NavLink>
+        </div>
       </header>
+
+      <footer className="Footer">
+        <ScrollUpButton style={{backgroundColor: "none", fill: "#999999"}}/>
+
+        <div className="Social-links">
+          <a href="mailto:tammyh0@uw.edu?Subject=Hi%20Tammy!" target="_top">Email</a>
+          <span>tammyh0@uw.edu //</span>
+          <a href="https://www.linkedin.com/in/ho-tammy/" target="_blank">LinkedIn</a> 
+          <span>//</span>
+          <a href="https://medium.com/@tammyh0" target="_blank">Medium</a> 
+        </div>
+
+        <div className="Copyright">
+          Built from scratch with React ✩ Updated March 2020.
+        </div>
+
+        <div className="Bottom-bar"></div>
+      </footer>
+
     </div>
   );
+  </BrowserRouter>
 }
 
 export default App;
